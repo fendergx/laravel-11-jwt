b7e6afc04361da7dc77b28c2355dc6ea65bb4b72
Francisco Gomez <francisco.gomez@salud.gob.sv>
2024-09-02 10:40:47 -0600

    adding laravel-passport

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Passport;
+
+Passport::hashClientSecrets();
 
 class AppServiceProvider extends ServiceProvider
 {
